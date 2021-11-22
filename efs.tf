0dbf165f8b3c9fedd70855b85050fa11e961acfe
Marat Kaziyev <m@mail.com>
2021-11-22 19:23:36 +0600

    added efs

---
+++ efs.tf
@@ -0,0 +1,25 @@
+resource "aws_efs_file_system" "ghost_content" {
+  creation_token = "ghost_content"
+
+  tags = {
+    Name = "ghost_content"
+  }
+}
+
+resource "aws_efs_mount_target" "efs_mnt_a" {
+  file_system_id  = aws_efs_file_system.ghost_content.id
+  subnet_id       = aws_subnet.public_a.id
+  security_groups = [ aws_security_group.efs.vpc_id ]
+}
+
+resource "aws_efs_mount_target" "efs_mnt_b" {
+  file_system_id  = aws_efs_file_system.ghost_content.id
+  subnet_id       = aws_subnet.public_b.id
+  security_groups = [ aws_security_group.efs.vpc_id ]
+}
+
+resource "aws_efs_mount_target" "efs_mnt_c" {
+  file_system_id  = aws_efs_file_system.ghost_content.id
+  subnet_id       = aws_subnet.public_c.id
+  security_groups = [ aws_security_group.efs.vpc_id ]
+}
